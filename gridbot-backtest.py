683ae750d41a24fb39a36a1e80f366bd1f68ad11
k` <kylenewberry@gmail.com>
2022-04-14 11:32:02 -0400

    Added Dictionary

--- gridbot-backtest.py
+++ gridbot-backtest.py
@@ -2,11 +2,8 @@ import pandas as pd
 import glob
 
 list_of_files = glob.glob('DATAFILES/*.csv')
-
 for csv in list_of_files:
     df = pd.read_csv(csv, usecols=['high','low'])
-
-    # User Config
     money = 2000
     shares = 0
     fee = .001
@@ -27,11 +24,10 @@ for csv in list_of_files:
         'share_split_percent': 1,        
         },]
      
-    # Money & Share Split Calculation
     for i in range(len(user_config)):
         user_config[i]['money_split'] = money * user_config[i]['money_split_percent']
         user_config[i]['share_split'] = shares * user_config[i]['share_split_percent']
-    
+
     for index, row in df.iterrows():
         for i in range(len(user_config)):
             if user_config[i]['share_split'] == 0:
@@ -46,14 +42,6 @@ for csv in list_of_files:
                     print(f"sold {user_config[i]['share_split']} shares at {user_config[i]['sell_price']} for ${user_config[i]['money_split']} dollars")
                     user_config[i]['share_split'] = 0  
 
-
 print("\nSUCCESS...")
-print(f"Total Money: {money}")
-print(f"Number of Splits: {len(user_config)}")
-print(f"Money Split: {user_config[0]['money_split']}")
-#print(f"Buying at: {user_config['buy_price']}")
-#print(f"Selling at: {user_config['sell_price']}")
-#print(f"Maker/Taker Fee:", fee * 100)
-#print()
-#print (f"Shares Owned: {sum(share_split)}")
-#print(f"Revenue: {sum(money_split)}\n")
+print("Total Revenue:",(round(sum(item['money_split'] for item in user_config),2)))
+print("Total Shares:",(round(sum(item['share_split'] for item in user_config),2)),"\n")

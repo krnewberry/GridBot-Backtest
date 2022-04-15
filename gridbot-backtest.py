5eeccc5b7c1a91ec938df5b6a9fd11d88ab4b144
k` <kylenewberry@gmail.com>
2022-04-15 11:03:39 -0400

    updates

--- gridbot-backtest.py
+++ gridbot-backtest.py
@@ -3,20 +3,21 @@ import glob
 
 list_of_files = glob.glob('DATAFILES/*.csv')
 for csv in list_of_files:
-    df = pd.read_csv(csv, usecols=['date','high','low'], index_col='date')
+    df = pd.read_csv(csv, usecols=['date','high','low'],index_col='date')
 
-    money = 3000
+    money = 1000
     shares = 0
     fee = .001
     user_config = [
         {
-        'buy_price': 1,
-        'sell_price': 2,
+        'buy_price': 1.0002,
+        'sell_price': 1.089,
         'money_split': 0,
-        'money_split_percent': 0.333333,
+        'money_split_percent': 1,
         'share_split': 0,
         'share_split_percent': 1,
-        },{
+        },
+        {
         'buy_price': 2,
         'sell_price': 4,
         'money_split': 0,
@@ -36,18 +37,23 @@ for csv in list_of_files:
         user_config[i]['money_split'] = money * user_config[i]['money_split_percent']
         user_config[i]['share_split'] = shares * user_config[i]['share_split_percent']
 
+    print(csv, "\n")
+    f = open("log.txt", "a")
+    
     for index, row in df.iterrows():
         for i in range(len(user_config)):
             if user_config[i]['share_split'] == 0:
                 if row["low"] >= user_config[i]['buy_price']:
                     user_config[i]['share_split'] = (user_config[i]['money_split'] - (user_config[i]['money_split'] * fee)) / user_config[i]['buy_price']
-                    print(f"{df.index[i]}: PURCHASED: {round(user_config[i]['share_split'],2)} shares at ${round(user_config[i]['buy_price'],2)} with ${round(user_config[i]['money_split'],2)} dollars")
+                    print(f"{df.index[i]}: PURCHASED: {user_config[i]['share_split']} shares at ${user_config[i]['buy_price']} with ${user_config[i]['money_split']} dollars")
+                    f.write(f"{df.index[i]}: PURCHASED: {user_config[i]['share_split']} shares at ${user_config[i]['buy_price']} with ${user_config[i]['money_split']} dollars \n")
                     user_config[i]['money_split'] = 0
             else:
                 if row["high"] >= user_config[i]['sell_price']:
                     user_config[i]['money_split'] = user_config[i]['sell_price'] * user_config[i]['share_split']
                     user_config[i]['money_split'] = (user_config[i]['money_split'] - (user_config[i]['money_split'] * fee))
-                    print(f"{df.index[i]}: SOLD: {round(user_config[i]['share_split'],2)} shares at ${round(user_config[i]['sell_price'],2)} for ${round(user_config[i]['money_split'],2)} dollars")
+                    print(f"{df.index[i]}: SOLD: {user_config[i]['share_split']} shares at ${user_config[i]['sell_price']} for ${user_config[i]['money_split']} dollars")
+                    f.write(f"{df.index[i]}: SOLD: {user_config[i]['share_split']} shares at ${user_config[i]['sell_price']} for ${user_config[i]['money_split']} dollars \n")
                     user_config[i]['share_split'] = 0
 
 print("\nSUCCESS...\n")
